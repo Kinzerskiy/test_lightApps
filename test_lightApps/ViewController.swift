fde31c5fce1cdceafee40b3258d2264e9f6026b4
Anton <kinzzersky@gmail.com>
2024-10-26 09:20:40 +0300

    Initial Commit

---
+++ test_lightApps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test_lightApps
+//
+//  Created by Anton on 26.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
